1c95b2c2e2ef4cd0108d46fd7c9da711b5331b56
Maverick4310 <cdiaz@navitascredit.com>
2026-06-09 13:03:14 -0400

    Create normalize.js

---
+++ utils/normalize.js
@@ -0,0 +1,15 @@
+export function normalizeName(name) {
+
+    if (!name) {
+        return '';
+    }
+
+    return name
+        .toUpperCase()
+        .replace(/\bLLC\b/g, '')
+        .replace(/\bINC\b/g, '')
+        .replace(/\bCORP\b/g, '')
+        .replace(/\bCORPORATION\b/g, '')
+        .replace(/\bLTD\b/g, '')
+        .replace(/[^A-Z0-9]/g, '');
+}
