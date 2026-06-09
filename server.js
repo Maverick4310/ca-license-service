d4d9e852d4fd02cb89c5cbd8958467b5701a36e3
Maverick4310 <cdiaz@navitascredit.com>
2026-06-09 12:58:57 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import licenseRoutes from './routes/licenses.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/licenses', licenseRoutes);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Listening on ${port}`);
+});
